9780b03d9e92b15a72665baaac72568b2fe35945
Camper <65611991+camper342@users.noreply.github.com>
2020-11-20 11:32:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
 const tpoints = {};
-const prefix = "you can not invite me";
+const prefix = "1";
 const vpoints = {};
 const jimp = require("jimp");
 const client = new Discord.Client();
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity("You can not invite me", { type: "watching" });
+  client.user.setActivity("ڕیکلام بۆ منێرە", { type: "playing" });
   console.log("");
   console.log("");
   console.log(
@@ -33,283 +33,54 @@ client.on("ready", () => {
   console.log("");
   console.log("");
 });
-const fs = require("fs");
-const anti = JSON.parse(fs.readFileSync("./antigreff.json", "UTF8")); // create antigreff.json file with {} inside it
-client.login("NzczNDYzNTU0ODU4NjgwMzIw.X6JmEQ.o01xA3zAkIW8XVoHQBP7ug8_Pdk");
-//////////////////////////////////
-//anti kick
-let config = {
-  banL: 3,
-  channelL: 3,
-  roleL: 0,
-  kickL: 3,
-  time: 60 * 5
-};
 
 
-client.on("channelDelete", async channel => {
-  const entry1 = await channel.guild
-    .fetchAuditLogs({
-      type: "CHANNEL_DELETE"
-    })
-    .then(audit => audit.entries.first());
-  console.log(entry1.executor.username);
-  const entry = entry1.executor;
-  if (!anti[channel.guild.id + entry.id]) {
-    anti[channel.guild.id + entry.id] = {
-      actions: 1
-    };
 
-    setTimeout(() => {
-      anti[channel.guild.id + entry.id].actions = "0";
-    }, config.time * 1000);
-  } else {
-    anti[channel.guild.id + entry.id].actions = Math.floor(
-      anti[channel.guild.id + entry.id].actions + 1
-    );
-    console.log("TETS");
-    setTimeout(() => {
-      anti[channel.guild.id + entry.id].actions = "0";
-    }, config.time * 1000);
-    if (anti[channel.guild.id + entry.id].actions >= config.channelL) {
-      channel.guild.members
-        .get(entry.id)
-        .ban()
-        .catch(e =>
-          channel.guild.owner.send(
-            `**⇏ | ${entry.username} tried to delete many channels**`
-          )
-        );
-      anti[channel.guild.id + entry.id].actions = "0";
+client.on("message" , message => {
+
+if(message.channel.type === 'dm') {
+
+if (message.content.startsWith("https://discord.gg/")) {
+    
+  message.author.send(`daxoi`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+    
+  client.channels.get("779031554101346345").send(
+`> sent By  <@${message.author.id}> ✅ 
+${message.content}`)
+}
     }
-  }
+        })
+
+
+
+client.on('message', message => { 
+
+if (!message.guild) return; 
+
+if (message.content === prefix + 'wara') { 
+  
+ if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  
+if (message.member.voiceChannel) { 
+
+message.member.voiceChannel.join() 
+
+.then(connection => { 
+
+
+}) 
+
+.catch(console.log); 
+
+} else { 
+
+message.reply('**بڕۆ ڤۆیسیک**'); 
+
+} 
+
+} 
+
 });
 
-client.on("channelCreate", async channel => {
-  const entry1 = await channel.guild
-    .fetchAuditLogs({
-      type: "CHANNEL_CREATE"
-    })
-    .then(audit => audit.entries.first());
-  console.log(entry1.executor.username);
-  const entry = entry1.executor;
-  if (!anti[channel.guild.id + entry.id]) {
-    anti[channel.guild.id + entry.id] = {
-      actions: 1
-    };
+const.login("NzcyNDYzMjYwNDU1OTI3ODI4.X57CeA.sja7wTOr6pNO6fH5yjc7zw9Tkcg");
 
-    setTimeout(() => {
-      anti[channel.guild.id + entry.id].actions = "0";
-    }, config.time * 1000);
-  } else {
-    anti[channel.guild.id + entry.id].actions = Math.floor(
-      anti[channel.guild.id + entry.id].actions + 1
-    );
-    console.log("TETS");
-    setTimeout(() => {
-      anti[channel.guild.id + entry.id].actions = "0";
-    }, config.time * 1000);
-    if (anti[channel.guild.id + entry.id].actions >= config.channelL) {
-      channel.guild.members
-        .get(entry.id)
-        .ban()
-        .catch(e =>
-          channel.guild.owner.send(
-            `**⇏ | ${entry.username} tried to delete many channels**`
-          )
-        );
-      anti[channel.guild.id + entry.id].actions = "0";
-    }
-  }
-});
-
-client.on("roleDelete", async channel => {
-  const entry1 = await channel.guild
-    .fetchAuditLogs({
-      type: "ROLE_DELETE"
-    })
-    .then(audit => audit.entries.first());
-  console.log(entry1.executor.username);
-  const entry = entry1.executor;
-  if (!anti[channel.guild.id + entry.id]) {
-    anti[channel.guild.id + entry.id] = {
-      actions: 1
-    };
-    setTimeout(() => {
-      anti[channel.guild.id + entry.id].actions = "0";
-    }, config.time * 1000);
-  } else {
-    anti[channel.guild.id + entry.id].actions = Math.floor(
-      anti[channel.guild.id + entry.id].actions + 1
-    );
-    console.log("TETS");
-    setTimeout(() => {
-      anti[channel.guild.id + entry.id].actions = "0";
-    }, config.time * 1000);
-    if (anti[channel.guild.id + entry.id].actions >= config.roleL) {
-      channel.guild.members
-        .get(entry.id)
-        .ban()
-        .catch(e =>
-          channel.guild.owner.send(
-            `**⇏ | ${entry.username} tried to delete many roles **`
-          )
-        );
-    }
-  }
-});
-
-client.on("roleCreate", async channel => {
-  const entry1 = await channel.guild
-    .fetchAuditLogs({
-      type: "ROLE_CREATE"
-    })
-    .then(audit => audit.entries.first());
-  console.log(entry1.executor.username);
-  const entry = entry1.executor;
-  if (!anti[channel.guild.id + entry.id]) {
-    anti[channel.guild.id + entry.id] = {
-      actions: 1
-    };
-    setTimeout(() => {
-      anti[channel.guild.id + entry.id].actions = "0";
-    }, config.time * 1000);
-  } else {
-    anti[channel.guild.id + entry.id].actions = Math.floor(
-      anti[channel.guild.id + entry.id].actions + 1
-    );
-    setTimeout(() => {
-      anti[channel.guild.id + entry.id].actions = "0";
-    }, config.time * 1000);
-    if (anti[channel.guild.id + entry.id].actions >= config.roleL) {
-      channel.guild.members
-        .get(entry.id)
-        .ban()
-        .catch(e =>
-          channel.guild.owner.send(
-            `**⇏ | ${entry.username} tried to create many roles**`
-          )
-        );
-      anti[channel.guild.id + entry.id].actions = "0";
-    }
-  }
-});
-
-client.on("guildBanAdd", async (guild, user) => {
-  const entry1 = await guild
-    .fetchAuditLogs({
-      type: "MEMBER_BAN_ADD"
-    })
-    .then(audit => audit.entries.first());
-  console.log(entry1.executor.username);
-  const entry = entry1.executor;
-  if (!anti[guild.id + entry.id]) {
-    anti[guild.id + entry.id] = {
-      actions: 1
-    };
-    setTimeout(() => {
-      anti[guild.id + entry.id].actions = "0";
-    }, config.time * 1000);
-  } else {
-    anti[guild.id + entry.id].actions = Math.floor(
-      anti[guild.id + entry.id].actions + 1
-    );
-    setTimeout(() => {
-      anti[guild.id + entry.id].actions = "0";
-    }, config.time * 1000);
-    if (anti[guild.id + entry.id].actions >= config.banL) {
-      guild.members
-        .get(entry.id)
-        .ban()
-        .catch(e =>
-          guild.owner.send(
-            `**⇏ | ${entry.username} Tried to ban all memebers **`
-          )
-        );
-      anti[guild.id + entry.id].actions = "0";
-    }
-  }
-});
-
-client.on("guildKickAdd", async (guild, user) => {
-  const entry1 = await guild
-    .fetchAuditLogs({
-      type: "MEMBER_KICK"
-    })
-    .then(audit => audit.entries.first());
-  console.log(entry1.executor.username);
-  const entry = entry1.executor;
-  if (!anti[guild.id + entry.id]) {
-    anti[guild.id + entry.id] = {
-      actions: 1
-    };
-    setTimeout(() => {
-      anti[guild.id + entry.id].actions = "0";
-    }, config.time * 1000);
-  } else {
-    anti[guild.id + entry.id].actions = Math.floor(
-      anti[guild.id + entry.id].actions + 1
-    );
-    console.log("TETS");
-    setTimeout(() => {
-      anti[guild.id + entry.id].actions = "0";
-    }, config.time * 1000);
-    if (anti[guild.id + entry.id].actions >= config.kickL) {
-      guild.members
-        .get(entry.id)
-        .ban()
-        .catch(e =>
-          guild.owner.send(
-            `**⇏ | ${entry.username} tried to ban all memebers **`
-          )
-        );
-      anti[guild.id + entry.id].actions = "0";
-    }
-  }
-});
-
-client.on("guildMemberRemove", async member => {
-  const entry1 = await member.guild
-    .fetchAuditLogs()
-    .then(audit => audit.entries.first());
-  if (entry1.action === "MEMBER_KICK") {
-    const entry2 = await member.guild
-      .fetchAuditLogs({
-        type: "MEMBER_KICK"
-      })
-      .then(audit => audit.entries.first());
-    const entry = entry2.executor;
-    if (!anti[member.guild.id + entry.id]) {
-      anti[member.guild.id + entry.id] = {
-        actions: 1
-      };
-      setTimeout(() => {
-        anti[member.guild.id + entry.id].actions = "0";
-      }, config.time * 1000);
-    } else {
-      anti[member.guild.id + entry.id].actions = Math.floor(
-        anti[member.guild.id + entry.id].actions + 1
-      );
-      console.log("TETS");
-      setTimeout(() => {
-        anti[member.guild.id + entry.id].actions = "0";
-      }, config.time * 1000);
-      if (anti[member.guild.id + entry.id].actions >= config.kickL) {
-        member.guild.members
-          .get(entry.id)
-          .ban()
-          .catch(e =>
-            member.owner.send(
-              `**⇏ | ${entry.username} tried to ban all memebers **`
-            )
-          );
-        anti[member.guild.id + entry.id].actions = "0";
-      }
-    }
-  }
-});
-
-client.on("guildMemberAdd", member => {
-  if (!member.guild) return;
-  if (member.author.bot) return member.kick();
-});
